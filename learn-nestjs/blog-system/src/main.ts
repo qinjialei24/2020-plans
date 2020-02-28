357a1acbd8451aa0138103212ef8a5c2ffbf1141
qinjialei <jialei.qin@thoughtworks.com>
2020-02-28 21:36:10 +0800

    ignore dist dir

--- learn-nestjs/blog-system/src/main.ts
+++ learn-nestjs/blog-system/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import * as mongoose from "mongoose";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+   const app = await NestFactory.create(AppModule);
 
   mongoose.connect('mongodb://localhost/nest-blog-api', {
     useNewUrlParser: true,
