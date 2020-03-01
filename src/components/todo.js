a2271345571ace67d955e34bbb60a03799572f8e
qinjialei <jialei.qin@thoughtworks.com>
2020-03-01 10:56:58 +0800

    update markdown

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TodoItem } from "./todoItem";
+import { TodoItem } from "./TodoItem";
 
 const Todo = (props) => {
   const { inputValue, inputChange, addItem, list } = props
