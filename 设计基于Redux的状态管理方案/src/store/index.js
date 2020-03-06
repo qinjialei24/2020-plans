74204c4063fcb8df390d6df2607b88a1a25c9085
qinjialei <jialei.qin@thoughtworks.com>
2020-03-06 22:06:34 +0800

    Update

--- 设计基于Redux的状态管理方案/src/store/index.js
+++ 设计基于Redux的状态管理方案/src/store/index.js
@@ -1,30 +1,23 @@
 import { createStore, combineReducers } from "redux";
 import todo from "./modules/todo";
 import counter from "./modules/counter";
-import { init } from "./util";
-
-const setActionToStore = (store, reducerModules) => {
-  Object.keys(reducerModules).forEach(moduleName => {
-    store[moduleName] = reducerModules[moduleName].action
-  })
-}
+import { setActionToStore } from "./util";
 
 const reducerModules = {
   todo,
   counter,
 }
 
-const _store = createStore(
+const store = createStore(
   combineReducers(reducerModules),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-setActionToStore(_store, reducerModules)
+setActionToStore(store, reducerModules)
 
-const _dispatch = _store.dispatch
+const _dispatch = store.dispatch
 
-_store.dispatch = (type, data) => _dispatch({ type, data })
+store.dispatch = (type, data) => _dispatch({ type, data })
 
-const store = init(_store)
 
 export default store
--- 设计基于Redux的状态管理方案/src/store/util.js
+++ 设计基于Redux的状态管理方案/src/store/util.js
@@ -1,15 +1,13 @@
 import produce from "immer"
 
-// /* TODO: */ reducer 和 effect 重名时 给予警告
-// /* TODO: */ 考虑 reducer 和 effect 不存在 或者为空对象的情况
 export const getReducerEffectMap = (reducerAndEffect, namespace) => Object.keys(reducerAndEffect)
   .reduce((actionMap, actionName) => ({
     ...actionMap,
     [actionName]: namespace + '/' + actionName
   }), {})
 
-const getKey = (str, flag) => {
-  const i = str.indexOf(flag)
+const getKey = (str) => {
+  const i = str.indexOf('/')
   return str.substring(i + 1, str.length + 1)
 }
 
@@ -17,40 +15,8 @@ export const handleActions = ({ state, action, reducer, namespace = '' }) =>
   Object.keys(reducer)
     .map(key => namespace + '/' + key)
     .includes(action.type)
-    ? produce(state, draft => reducer[getKey(action.type, '/')](draft, action))
+    ? produce(state, draft => reducer[getKey(action.type)](draft, action))
     : state
-
-// export const formatModel = ({ state, action, reducer, effect, namespace = '' }) =>
-//   Object.keys(reducer)
-//     .map(key => namespace + '/' + key)
-//     .includes(action.type)
-//     ? produce(state, draft => reducer[getKey(action.type, '/')](draft, action))
-//     : state
-
-export const formatModel = ({ state, action, reducer, effect, namespace = '' }) => {
-  const reducerKey = Object.keys(reducer).map(key => namespace + '/' + key)
-  const effectKey = Object.keys(effect).map(key => namespace + '/' + key)
-
-  // 触发的是 reducer
-  if (reducerKey.includes(action.type)) {
-    return produce(state, draft => reducer[getKey(action.type, '/')](draft, action))
-  }
-
-  // 触发的是 effect
-  // if (effectKey.includes(action.type)) {
-  //   const effectFn = effect[getKey(action.type, '/')]
-  //   effectFn(reducer, action.data, state)
-  // }
-  else {
-    return state
-  }
-}
-
-
-export const handleEffect = (effect = {}) => {
-
-}
-
 
 export const createModel = (model) => {
   const { reducer, namespace } = model
@@ -65,14 +31,9 @@ export const createModel = (model) => {
   return fn
 }
 
-// 增加 modules 属性 存储所有模块
-const initModules = store => {
-  // store[modules] = {}
+
+export const setActionToStore = (store, reducerModules) => {
+  Object.keys(reducerModules).forEach(moduleName => {
+    store[moduleName] = reducerModules[moduleName].action
+  })
 }
-
-export const init = store => {
-  // initModules(store)
-
-  return store
-
-} 
