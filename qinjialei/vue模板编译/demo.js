a13a1e8da628273c02e4b066f37b44131369c65d
qinjialei <jialei.qin@thoughtworks.com>
2020-03-26 08:29:38 +0800

    添加vue模板编译demo

---
+++ qinjialei/vue模板编译/demo.js
@@ -0,0 +1,28 @@
+const ncname = '[a-zA-Z_][\\w\\-\\.]*'
+const qnameCapture = `((?:${ncname}\\:)?${ncname})`
+const startTagOpen = new RegExp(`^<${qnameCapture}`)
+const startTagClose = /^\s*(\/?)>/
+let html = `<div></div>`
+let index = 0
+const start = html.match(startTagOpen)
+
+const match = {
+  tagName: start[1],
+  attrs: [],
+  start: 0
+}
+html = html.substring(start[0].length)
+index += start[0].length
+let end, attr
+while (!(end = html.match(startTagClose)) && (attr = html.match(attribute))) {
+  html = html.substring(attr[0].length)
+  index += attr[0].length
+  match.attrs.push(attr)
+}
+if (end) {
+  match.unarySlash = end[1]
+  html = html.substring(end[0].length)
+  index += end[0].length
+  match.end = index
+}
+console.log(match)
