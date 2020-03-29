e0efc2ca4dfdf1e4345021d308459366f06df140
qinjialei <jialei.qin@thoughtworks.com>
2020-03-29 22:11:56 +0800

    use useEffect

--- qinjialei/优化Redux/src/components/counter.js
+++ qinjialei/优化Redux/src/components/counter.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Counter(props) {
+  const [count, setCount] = useState(0)
+  useEffect(() => {
+    document.title = "componentDidMount" + count;
+  }, [count]);
+
   return (
     <div>
-      <h3>counter value is:</h3>
-      <hr />
-      <button onClick={() => { props.add(111) }}>+</button>
-      <button onClick={props.minus}>-</button>
-      <h4>{props.count}</h4>
+      <div>{count}</div>
+      <button onClick={() => setCount(count + 1)}>+</button>
     </div>
   )
 }
