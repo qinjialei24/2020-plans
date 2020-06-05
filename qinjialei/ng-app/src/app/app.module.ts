d746b70bff26d45c918263849b3d9b6d7a26bc1a
qinjialei <jialei.qin@thoughtworks.com>
2020-06-05 23:05:27 +0800

    Update

--- qinjialei/ng-app/src/app/app.module.ts
+++ qinjialei/ng-app/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { NZ_I18N, zh_CN } from 'ng-zorro-antd';
 import { CoreModule } from './core/core.module';
 import { ShareModule } from './share/share.module';
 
