b36442c5e5bfb696c3432ba44544c2719df67d2d
qinjialei <jialei.qin@thoughtworks.com>
2020-06-06 00:01:40 +0800

    Update

--- qinjialei/ng-app/src/app/pages/pages.module.ts
+++ qinjialei/ng-app/src/app/pages/pages.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ShareModule } from '../share/share.module';
+import { HomeModule } from './home/home.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
-    ShareModule
+    ShareModule,
+    HomeModule
+  ],
+  exports: [
+    HomeModule
   ]
 })
 export class PagesModule { }
